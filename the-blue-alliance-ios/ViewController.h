39f502803a104e2cf399729f2dc224eb6a42807b
Donald Pinckney <donald_pinckney@icloud.com>
2014-05-05 23:40:37 -0700

    Initial Commit

---
+++ the-blue-alliance-ios/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  the-blue-alliance-ios
+//
+//  Created by Donald Pinckney on 5/5/14.
+//  Copyright (c) 2014 The Blue Alliance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ the-blue-alliance-iosTests/the_blue_alliance_iosTests.m
@@ -0,0 +1,34 @@
+//
+//  the_blue_alliance_iosTests.m
+//  the-blue-alliance-iosTests
+//
+//  Created by Donald Pinckney on 5/5/14.
+//  Copyright (c) 2014 The Blue Alliance. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface the_blue_alliance_iosTests : XCTestCase
+
+@end
+
+@implementation the_blue_alliance_iosTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
