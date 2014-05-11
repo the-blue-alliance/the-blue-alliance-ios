17642f032df0f8b41a0662ff2e2fc8f0e390c5a7
Donald Pinckney <donald_pinckney@icloud.com>
2014-05-11 15:32:16 -0700

    Implemented sections in the Events controller

--- the-blue-alliance-ios/EventsViewController.m
+++ the-blue-alliance-ios/EventsViewController.m
@@ -21,22 +21,23 @@
     
     NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:@"Event"];
     fetchRequest.predicate = nil;
-    fetchRequest.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"start_date" ascending:NO]];
+    fetchRequest.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"start_date" ascending:NO], [NSSortDescriptor sortDescriptorWithKey:@"name" ascending:YES]];
     
     self.fetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest
                                                                                                managedObjectContext:context
-                                                                                                 sectionNameKeyPath:nil
+                                                                                                 sectionNameKeyPath:@"start_date"
                                                                                                           cacheName:nil];
 }
 
 - (void) viewDidLoad
 {
     [super viewDidLoad];
-    
+        
     // TODO: Replace with some customized cell
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"Event Cell"];
 }
 
+#pragma mark - UITableViewDataSource
 - (UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Event Cell" forIndexPath:indexPath];
@@ -46,6 +47,21 @@
     cell.textLabel.text = event.name;
     
     return cell;
+}
+
+- (NSString *) tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    Event *event = [self.fetchedResultsController objectAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:section]];
+    
+    NSDateFormatter *friendlyFormatter = [[NSDateFormatter alloc] init];
+    friendlyFormatter.dateStyle = NSDateFormatterMediumStyle;
+    return [friendlyFormatter stringFromDate:event.start_date];
+}
+
+// Disable section indexing
+- (NSArray *) sectionIndexTitlesForTableView:(UITableView *)tableView
+{
+    return nil;
 }
 
 @end
