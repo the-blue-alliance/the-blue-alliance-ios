2f2edb8bc4ef44c31fd60b7190adad5e4715c9a3
Zachary Orr <zachorr139@gmail.com>
2014-05-23 00:14:02 -0400

    Adds documentation for MenuViewController

--- the-blue-alliance-ios/MenuViewController.h
+++ the-blue-alliance-ios/MenuViewController.h
@@ -8,18 +8,38 @@
 
 #import <UIKit/UIKit.h>
 
+/** `MenuViewController`'s delegate methods—defined by the MenuViewControllerDelegate protocol
+ *  call menuViewController:didSelectMenuItem: when a new menu item is selected
+ */
 @class MenuViewController;
 @protocol MenuViewControllerDelegate <NSObject>
 
+/** Called when a a menu item is tapped
+ *
+ * @param menu The menu object an item was selected on
+ * @param menuItem The object selected
+ */
 - (void) menuViewController:(MenuViewController *)menu didSelectMenuItem:(NSString *)menuItem;
 
 @end
 
+/** `MenuViewController` is the view controller for the sidebar menu
+ */
 @interface MenuViewController : UIViewController
 
+/** The object that is notified when a menu item is selected
+ */
 @property (nonatomic, weak) id<MenuViewControllerDelegate> delegate;
+
+/** A list of items that will be displayed in the menu
+ */
 @property (nonatomic, copy) NSArray *menuItems;
 
-- (instancetype) initWithMenuItems:(NSArray *)items;
+/** Initilizes the MenuViewController with an array of items
+ *
+ * @param items An array of NSStrings to show in the menu
+ * @return An initilized MenuViewController
+ */
+- (instancetype)initWithMenuItems:(NSArray *)items;
 
 @end
