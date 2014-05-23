3acd27d25447b01cdb863269d20004321353980f
Zachary Orr <zachorr139@gmail.com>
2014-05-23 00:21:29 -0400

    Documentation for EventsViewController

--- the-blue-alliance-ios/EventsViewController.h
+++ the-blue-alliance-ios/EventsViewController.h
@@ -9,8 +9,15 @@
 #import "SearchableCoreDataTableViewController.h"
 #import "YearSelectView.h"
 
+/** `EventsViewController` is The table view that lists all events for a season. Events are
+ *  ordered by week/event type (Week %d, Offseason, Preseason, etc). The can be filtered down
+ *  by searching for an event name or an event code. Which year's data to display can be 
+ *  changed using the YearSelect.
+ */
 @interface EventsViewController : SearchableCoreDataTableViewController <YearSelect>
 
+/** The context to be used for accessing Core Data
+ */
 @property (nonatomic, strong) NSManagedObjectContext *context;
 
 @end
