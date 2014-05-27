69788dcb581b9cea69fde3947eda64acaab613df
Donald Pinckney <donald_pinckney@icloud.com>
2014-05-26 23:05:10 -0700

    Added icons, and added location text, and made both date and location buttons.

--- the-blue-alliance-ios/EventInfoViewController.m
+++ the-blue-alliance-ios/EventInfoViewController.m
@@ -14,27 +14,68 @@
 
 @implementation EventInfoViewController
 
-
-- (void) viewDidLoad
+- (void)viewDidLoad
 {
     [super viewDidLoad];
     
     self.view = [[UIView alloc] init]; // For some reason this needs to be here, otherwise autolayout freaks out and moves subviews...
     self.view.backgroundColor = [UIColor whiteColor];
     
-    
-    UILabel *dateLabel = [[UILabel alloc] initForAutoLayout];
-    [self.view addSubview:dateLabel];
-    [dateLabel autoPinEdgeToSuperviewEdge:ALEdgeLeading withInset:20];
-    [dateLabel autoPinEdgeToSuperviewEdge:ALEdgeTop withInset:20];
+    // Create date label
+    UIImage *calendarImage = [UIImage imageNamed:@"calendar"];
+    UIImageView *calendar = [[UIImageView alloc] initWithImage:calendarImage];
+    calendar.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:calendar];
+    [calendar autoPinEdgeToSuperviewEdge:ALEdgeLeft withInset:20];
+    [calendar autoPinEdgeToSuperviewEdge:ALEdgeTop withInset:20];
+    [calendar autoSetDimensionsToSize:CGSizeMake(roundf(calendarImage.size.width * 0.8), roundf(calendarImage.size.height * 0.8))];
+
+    UIButton *dateButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    dateButton.tintColor = [UIColor TBANavigationBarColor];
+    dateButton.translatesAutoresizingMaskIntoConstraints = NO;
+    [dateButton addTarget:self action:@selector(dateTapped:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:dateButton];
+    [dateButton autoPinEdge:ALEdgeLeft toEdge:ALEdgeRight ofView:calendar withOffset:4];
+    [dateButton autoAlignAxis:ALAxisHorizontal toSameAxisOfView:calendar];
     
     NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
     formatter.dateStyle = NSDateFormatterMediumStyle;
     
-    dateLabel.text = [NSString stringWithFormat:@"%@ to %@", [formatter stringFromDate:self.event.start_date], [formatter stringFromDate:self.event.end_date]];
-    dateLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleSubheadline];
+    NSString *dateText = [NSString stringWithFormat:@"%@ to %@", [formatter stringFromDate:self.event.start_date], [formatter stringFromDate:self.event.end_date]];
+    [dateButton setTitle:dateText forState:UIControlStateNormal];
+    
+    // Create location label
+    UIImage *locationImage = [UIImage imageNamed:@"location"];
+    UIImageView *location = [[UIImageView alloc] initWithImage:locationImage];
+    location.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:location];
+    [location autoPinEdgeToSuperviewEdge:ALEdgeLeft withInset:20];
+    [location autoPinEdge:ALEdgeTop toEdge:ALEdgeBottom ofView:calendar withOffset:4];
+    [location autoSetDimensionsToSize:CGSizeMake(roundf(locationImage.size.width * 0.8), roundf(locationImage.size.height * 0.8))];
+
+    UIButton *locationLabel = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    locationLabel.tintColor = [UIColor TBANavigationBarColor];
+    locationLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    [locationLabel addTarget:self action:@selector(locationTapped:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:locationLabel];
+    [locationLabel autoPinEdge:ALEdgeLeft toEdge:ALEdgeRight ofView:location withOffset:4];
+    [locationLabel autoAlignAxis:ALAxisHorizontal toSameAxisOfView:location];
+
+    [locationLabel setTitle:self.event.location forState:UIControlStateNormal];
+    
 
 }
+
+- (void)dateTapped:(UIButton *)button
+{
+    NSLog(@"Date tapped... save event to calendar?");
+}
+
+- (void)locationTapped:(UIButton *)button
+{
+    NSLog(@"Location tapped... open in maps?");
+}
+
 
 
 @end
