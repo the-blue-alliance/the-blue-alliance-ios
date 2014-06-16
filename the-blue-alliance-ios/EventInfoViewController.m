459b6faf1124976653e39d868f8d925014ee3394
Donald Pinckney <donaldpinckney@donalds-mbp.gateway.pace.com>
2014-06-15 17:52:03 -0700

    Implemented basic map of venue location for new event info page.

--- the-blue-alliance-ios/EventInfoViewController.m
+++ the-blue-alliance-ios/EventInfoViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "EventInfoViewController.h"
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
 
 @interface EventInfoViewController ()
 
@@ -21,88 +23,19 @@
     self.view = [[UIView alloc] init]; // For some reason this needs to be here, otherwise autolayout freaks out and moves subviews...
     self.view.backgroundColor = [UIColor whiteColor];
     
-    // Create date label
-    UIImage *calendarImage = [UIImage imageNamed:@"calendar"];
-    UIImageView *calendar = [[UIImageView alloc] initWithImage:calendarImage];
-    calendar.translatesAutoresizingMaskIntoConstraints = NO;
-    [self.view addSubview:calendar];
-    [calendar autoPinEdgeToSuperviewEdge:ALEdgeLeft withInset:20];
-    [calendar autoPinEdgeToSuperviewEdge:ALEdgeTop withInset:20];
-    [calendar autoSetDimensionsToSize:CGSizeMake(roundf(calendarImage.size.width * 0.8), roundf(calendarImage.size.height * 0.8))];
+    MKMapView *mapView = [[MKMapView alloc] initForAutoLayout];
+    [self.view addSubview:mapView];
+    [mapView autoPinEdgeToSuperviewEdge:ALEdgeLeft withInset:0];
+    [mapView autoPinEdgeToSuperviewEdge:ALEdgeRight withInset:0];
+    [mapView autoSetDimension:ALDimensionHeight toSize:240];
+    mapView.backgroundColor = [UIColor redColor];
 
-    UIButton *dateButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    dateButton.tintColor = [UIColor TBANavigationBarColor];
-    dateButton.translatesAutoresizingMaskIntoConstraints = NO;
-    [dateButton addTarget:self action:@selector(dateTapped:) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:dateButton];
-    [dateButton autoPinEdge:ALEdgeLeft toEdge:ALEdgeRight ofView:calendar withOffset:4];
-    [dateButton autoAlignAxis:ALAxisHorizontal toSameAxisOfView:calendar];
-    
-    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
-    formatter.dateStyle = NSDateFormatterMediumStyle;
-    
-    NSString *dateText = [NSString stringWithFormat:@"%@ to %@", [formatter stringFromDate:self.event.start_date], [formatter stringFromDate:self.event.end_date]];
-    [dateButton setTitle:dateText forState:UIControlStateNormal];
-    
-    // Create location label
-    UIImage *locationImage = [UIImage imageNamed:@"location"];
-    UIImageView *location = [[UIImageView alloc] initWithImage:locationImage];
-    location.translatesAutoresizingMaskIntoConstraints = NO;
-    [self.view addSubview:location];
-    [location autoPinEdgeToSuperviewEdge:ALEdgeLeft withInset:20];
-    [location autoPinEdge:ALEdgeTop toEdge:ALEdgeBottom ofView:calendar withOffset:4];
-    [location autoSetDimensionsToSize:CGSizeMake(roundf(locationImage.size.width * 0.8), roundf(locationImage.size.height * 0.8))];
-
-    UIButton *locationButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    locationButton.tintColor = [UIColor TBANavigationBarColor];
-    locationButton.translatesAutoresizingMaskIntoConstraints = NO;
-    [locationButton addTarget:self action:@selector(locationTapped:) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:locationButton];
-    [locationButton autoPinEdge:ALEdgeLeft toEdge:ALEdgeRight ofView:location withOffset:4];
-    [locationButton autoAlignAxis:ALAxisHorizontal toSameAxisOfView:location];
-
-    [locationButton setTitle:self.event.location forState:UIControlStateNormal];
-    
-    
-    
-    // Create website button links row
-    UIButton *website = [[UIButton alloc] initForAutoLayout];
-    [website setImage:[UIImage imageNamed:@"website"] forState:UIControlStateNormal];
-    [self.view addSubview:website];
-    
-    UIButton *twitter = [[UIButton alloc] initForAutoLayout];
-    [twitter setImage:[UIImage imageNamed:@"twitter"] forState:UIControlStateNormal];
-    [self.view addSubview:twitter];
-    
-    UIButton *youtube = [[UIButton alloc] initForAutoLayout];
-    [youtube setImage:[UIImage imageNamed:@"youtube"] forState:UIControlStateNormal];
-    [self.view addSubview:youtube];
-    
-    UIButton *chiefdelphi = [[UIButton alloc] initForAutoLayout];
-    [chiefdelphi setImage:[UIImage imageNamed:@"chiefdelphi"] forState:UIControlStateNormal];
-    [self.view addSubview:chiefdelphi];
-    
-    UIButton *usfirst = [[UIButton alloc] initForAutoLayout];
-    [usfirst setImage:[UIImage imageNamed:@"usfirst"] forState:UIControlStateNormal];
-    [self.view addSubview:usfirst];
-    
-    // Center youtube icon
-    [youtube autoAlignAxisToSuperviewAxis:ALAxisVertical];
-    [youtube autoPinEdge:ALEdgeTop toEdge:ALEdgeBottom ofView:locationButton withOffset:0];
-    
-    // Center other icons vertically to youtube
-    [website autoAlignAxis:ALAxisHorizontal toSameAxisOfView:youtube];
-    [twitter autoAlignAxis:ALAxisHorizontal toSameAxisOfView:youtube];
-    [chiefdelphi autoAlignAxis:ALAxisHorizontal toSameAxisOfView:youtube];
-    [usfirst autoAlignAxis:ALAxisHorizontal toSameAxisOfView:youtube];
-
-    // Connect other icons end-to-end
-    const int iconHorizontalSpacing = 16;
-    [website autoPinEdge:ALEdgeRight toEdge:ALEdgeLeft ofView:twitter withOffset:-iconHorizontalSpacing];
-    [twitter autoPinEdge:ALEdgeRight toEdge:ALEdgeLeft ofView:youtube withOffset:-iconHorizontalSpacing];
-    [youtube autoPinEdge:ALEdgeRight toEdge:ALEdgeLeft ofView:chiefdelphi withOffset:-iconHorizontalSpacing];
-    [chiefdelphi autoPinEdge:ALEdgeRight toEdge:ALEdgeLeft ofView:usfirst withOffset:-iconHorizontalSpacing];
-    
+    CLGeocoder *geocoder = [[CLGeocoder alloc] init];
+    [geocoder geocodeAddressString:self.event.location completionHandler:^(NSArray *placemarks, NSError *error) {
+        CLCircularRegion *region = (CLCircularRegion *)[(CLPlacemark *)[placemarks firstObject] region];
+        MKCoordinateRegion coordRegion = MKCoordinateRegionMakeWithDistance(region.center, 2*region.radius, 2*region.radius);
+        mapView.region = coordRegion;
+    }];
 
 }
 
