a2074f61befd7d8f006d1b92915e4b6971f8a5d6
Donald Pinckney <donald_pinckney@icloud.com>
2014-09-11 16:54:53 -0700

    Implemented proper UI for the social media buttons on the team page.

--- the-blue-alliance-ios/TBASocialButtonContainer.m
+++ the-blue-alliance-ios/TBASocialButtonContainer.m
@@ -30,28 +30,36 @@
     // Create views
     UIButton *websiteButton = [UIButton buttonWithType:UIButtonTypeCustom];
     websiteButton.translatesAutoresizingMaskIntoConstraints = NO;
-    websiteButton.backgroundColor = [UIColor purpleColor];
+    websiteButton.backgroundColor = [UIColor colorWithRed:77/255.0 green:79/255.0 blue:135/255.0 alpha:1.0];
+    [websiteButton setShowsTouchWhenHighlighted:YES];
+    [websiteButton setImage:[UIImage imageNamed:@"website-white"] forState:UIControlStateNormal];
     websiteButton.tag = TBASocialButtonContainerButtonTypeWebsite;
     [websiteButton addTarget:self action:@selector(buttonTapped:) forControlEvents:UIControlEventTouchUpInside];
     [self addSubview:websiteButton];
     
     UIButton *twitterButton = [UIButton buttonWithType:UIButtonTypeCustom];
     twitterButton.translatesAutoresizingMaskIntoConstraints = NO;
-    twitterButton.backgroundColor = [UIColor blueColor];
+    twitterButton.backgroundColor = [UIColor colorWithRed:89/255.0 green:173/255.0 blue:235/255.0 alpha:1.0];
+    [twitterButton setShowsTouchWhenHighlighted:YES];
+    [twitterButton setImage:[UIImage imageNamed:@"twitter-white"] forState:UIControlStateNormal];
     twitterButton.tag = TBASocialButtonContainerButtonTypeTwitter;
     [twitterButton addTarget:self action:@selector(buttonTapped:) forControlEvents:UIControlEventTouchUpInside];
     [self addSubview:twitterButton];
     
     UIButton *youtubeButton = [UIButton buttonWithType:UIButtonTypeCustom];
     youtubeButton.translatesAutoresizingMaskIntoConstraints = NO;
-    youtubeButton.backgroundColor = [UIColor redColor];
+    youtubeButton.backgroundColor = [UIColor colorWithRed:203/255.0 green:35/255.0 blue:39/255.0 alpha:1.0];
+    [youtubeButton setShowsTouchWhenHighlighted:YES];
+    [youtubeButton setImage:[UIImage imageNamed:@"youtube-white"] forState:UIControlStateNormal];
     youtubeButton.tag = TBASocialButtonContainerButtonTypeYoutube;
     [youtubeButton addTarget:self action:@selector(buttonTapped:) forControlEvents:UIControlEventTouchUpInside];
     [self addSubview:youtubeButton];
     
     UIButton *chiefDelphiButton = [UIButton buttonWithType:UIButtonTypeCustom];
     chiefDelphiButton.translatesAutoresizingMaskIntoConstraints = NO;
-    chiefDelphiButton.backgroundColor = [UIColor orangeColor];
+    chiefDelphiButton.backgroundColor = [UIColor colorWithRed:253/255.0 green:136/255.0 blue:37/255.0 alpha:1.0];
+    [chiefDelphiButton setShowsTouchWhenHighlighted:YES];
+    [chiefDelphiButton setImage:[UIImage imageNamed:@"chiefdelphi-white"] forState:UIControlStateNormal];
     chiefDelphiButton.tag = TBASocialButtonContainerButtonTypeChiefDelphi;
     [chiefDelphiButton addTarget:self action:@selector(buttonTapped:) forControlEvents:UIControlEventTouchUpInside];
     [self addSubview:chiefDelphiButton];
