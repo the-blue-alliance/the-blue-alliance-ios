fc12516c7167108dc93487b65305679d91de3750
Brandon Dean <brandon.dean@eleostech.com>
2016-05-01 18:30:41 -0400

    Show play button for matches that have videos.
    Puts the match label and play button in their own vertical stack view, and
    hides the play stack view if there isn't a video available for the match.

--- the-blue-alliance-ios/TBAMatchTableViewCell.m
+++ the-blue-alliance-ios/TBAMatchTableViewCell.m
@@ -24,6 +24,7 @@
 @property (nonatomic, strong) IBOutlet UILabel *blueScoreLabel;
 
 @property (nonatomic, strong) IBOutlet UILabel *timeLabel;
+@property (nonatomic, strong) IBOutlet UIStackView *playIconStackView;
 
 @property (nonatomic, strong) NSArray<UIView *> *coloredViews;
 
@@ -82,6 +83,7 @@
 
 - (void)configureCell {
     self.matchNumberLabel.text = [self.match friendlyMatchName];
+    self.playIconStackView.hidden = ![self.match.videos count];;
     
     for (UIView *view in self.redStackView.arrangedSubviews) {
         if (view == self.redScoreLabel) {
