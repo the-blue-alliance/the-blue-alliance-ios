081a9b18071810825dfd7398ca72fc212356fd2e
Zach Orr <zachorr139@gmail.com>
2016-05-02 09:52:12 -0400

    Remove team sponsors for now

--- the-blue-alliance-ios/TBAInfoTableViewCell.m
+++ the-blue-alliance-ios/TBAInfoTableViewCell.m
@@ -56,14 +56,14 @@
             [self.infoStackView addArrangedSubview:nicknameLabel];
         }
         if (self.team.location) {
-            UILabel *locationLabel = [self subtitleLabelWithText:self.team.location];
+            UILabel *locationLabel = [self subtitleLabelWithText:[NSString stringWithFormat:@"from %@", self.team.location]];
             [self.infoStackView addArrangedSubview:locationLabel];
         }
-        if (self.team.name) {
-            UILabel *nameLabel = [self subtitleLabelWithText:self.team.name];
-            nameLabel.numberOfLines = 3;
-            [self.infoStackView addArrangedSubview:nameLabel];
-        }
+//        if (self.team.name) {
+//            UILabel *nameLabel = [self subtitleLabelWithText:self.team.name];
+//            nameLabel.numberOfLines = 3;
+//            [self.infoStackView addArrangedSubview:nameLabel];
+//        }
         if (self.team.motto) {
             UILabel *mottoLabel = [self subtitleLabelWithText:self.team.motto];
             [self.infoStackView addArrangedSubview:mottoLabel];
