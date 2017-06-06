2cad6d5b916aaf11d21976687c4ee03d3fde271d
Zach Orr <zachorr139@gmail.com>
2017-06-06 10:53:31 -0400

    Add event insights for 2016

--- js/app/views/EventInsights2016.js
+++ js/app/views/EventInsights2016.js
@@ -5,7 +5,126 @@ import {
     Text,
     View
 } from 'react-native';
+import TableSectionHeader from '../componets/TableSectionHeader';
+import InsightRow from '../componets/InsightRow';
+import { round } from '../helpers/number';
+import {
+  bonusStat,
+  highScoreString
+} from '../helpers/insights';
 
 export default class EventInsights2016 extends React.Component {
+  render() {
 
+    return (
+      <View>
+        {/* Match Stats */}
+        <TableSectionHeader>Match Stats</TableSectionHeader>
+
+        <InsightRow title='High Score'
+                    qual={'Quals: ' + highScoreString(this.props.qual.high_score)}
+                    playoff={'Playoffs: ' + highScoreString(this.props.playoff.high_score)}/>
+
+        <InsightRow title='Average Low Goal'
+                    qual={round(this.props.qual.average_low_goals)}
+                    playoff={round(this.props.playoff.average_low_goals)}/>
+
+        <InsightRow title='Average High Goal'
+                    qual={round(this.props.qual.average_high_goals)}
+                    playoff={round(this.props.playoff.average_high_goals)}/>
+
+        <InsightRow title='Average Match Score'
+                    qual={round(this.props.qual.average_score)}
+                    playoff={round(this.props.playoff.average_score)}/>
+
+        <InsightRow title='Average Winning Score'
+                    qual={round(this.props.qual.average_win_score)}
+                    playoff={round(this.props.playoff.average_win_score)}/>
+
+        <InsightRow title='Average Win Margin'
+                    qual={round(this.props.qual.average_win_margin)}
+                    playoff={round(this.props.playoff.average_win_margin)}/>
+
+        <InsightRow title='Average Auto Score'
+                    qual={round(this.props.qual.average_auto_score)}
+                    playoff={round(this.props.playoff.average_auto_score)}/>
+
+        <InsightRow title='Average Teleop Crossing Score'
+                    qual={round(this.props.qual.average_crossing_score)}
+                    playoff={round(this.props.playoff.average_crossing_score)}/>
+
+        <InsightRow title='Average Teleop Boulder Score'
+                    qual={round(this.props.qual.average_boulder_score)}
+                    playoff={round(this.props.playoff.average_boulder_score)}/>
+
+        <InsightRow title='Average Teleop Tower Score'
+                    qual={round(this.props.qual.average_tower_score)}
+                    playoff={round(this.props.playoff.average_tower_score)}/>
+        
+        <InsightRow title='Average Foul Score'
+                    qual={round(this.props.qual.average_foul_score)}
+                    playoff={round(this.props.playoff.average_foul_score)}/>
+
+        {/* Tower Stats */}
+        <TableSectionHeader>Tower Stats (# successful / # opportunities)</TableSectionHeader>
+
+        <InsightRow title='Challenges'
+                    qual={bonusStat(this.props.qual.challenges)}
+                    playoff={bonusStat(this.props.playoff.challenges)}/>
+
+        <InsightRow title='Scales'
+                    qual={bonusStat(this.props.qual.scales)}
+                    playoff={bonusStat(this.props.playoff.scales)}/>
+
+        <InsightRow title='Captures'
+                    qual={bonusStat(this.props.qual.captures)}
+                    playoff={bonusStat(this.props.playoff.captures)}/>
+
+        {/* Defense Stats */}
+        <TableSectionHeader>Defense Stats (# damaged / # opportunities)</TableSectionHeader>
+
+        <InsightRow title='Low Bar'
+                    qual={bonusStat(this.props.qual.LowBar)}
+                    playoff={bonusStat(this.props.playoff.LowBar)}/>
+
+        {/* These are grouped together by similar backgrounds on web */}
+        <InsightRow title='Cheval De Frise'
+                    qual={bonusStat(this.props.qual.A_ChevalDeFrise)}
+                    playoff={bonusStat(this.props.playoff.A_ChevalDeFrise)}/>
+
+        <InsightRow title='Portcullis'
+                    qual={bonusStat(this.props.qual.A_Portcullis)}
+                    playoff={bonusStat(this.props.playoff.A_Portcullis)}/>
+
+        <InsightRow title='Ramparts'
+                    qual={bonusStat(this.props.qual.B_Ramparts)}
+                    playoff={bonusStat(this.props.playoff.B_Ramparts)}/>
+
+        <InsightRow title='Moat'
+                    qual={bonusStat(this.props.qual.B_Moat)}
+                    playoff={bonusStat(this.props.playoff.B_Moat)}/>
+        
+        <InsightRow title='Sally Port'
+                    qual={bonusStat(this.props.qual.C_SallyPort)}
+                    playoff={bonusStat(this.props.playoff.C_SallyPort)}/>
+
+        <InsightRow title='Drawbridge'
+                    qual={bonusStat(this.props.qual.C_Drawbridge)}
+                    playoff={bonusStat(this.props.playoff.C_Drawbridge)}/>
+
+        <InsightRow title='Rough Terrain'
+                    qual={bonusStat(this.props.qual.D_RoughTerrain)}
+                    playoff={bonusStat(this.props.playoff.D_RoughTerrain)}/>
+
+        <InsightRow title='Rock Wall'
+                    qual={bonusStat(this.props.qual.D_RockWall)}
+                    playoff={bonusStat(this.props.playoff.D_RockWall)}/>
+        
+        <InsightRow title='Total Breaches'
+                    qual={bonusStat(this.props.qual.breaches)}
+                    playoff={bonusStat(this.props.playoff.breaches)}/>
+
+      </View>
+    );
+  } 
 }
--- js/app/views/EventInsights2017.js
+++ js/app/views/EventInsights2017.js
@@ -7,121 +7,106 @@ import {
 } from 'react-native';
 import TableSectionHeader from '../componets/TableSectionHeader';
 import InsightRow from '../componets/InsightRow';
+import { round } from '../helpers/number';
+import {
+  bonusStat,
+  highScoreString,
+} from '../helpers/insights';
 
 export default class EventInsights2017 extends React.Component {
-  
-  round(num) {
-    return (Math.round(num * 100) / 100).toFixed(2)
-  }
-
-  highScoreString(highScoreData) {
-    if (highScoreData.length != 3) {
-      return highScoreData.join(' ')
-    }
-    return highScoreData[0] + ' in ' + highScoreData[2]
-  }
-
-  bonusStat(bonusData) {
-    if (bonusData.length != 3) {
-      return bonusData.join(' / ')
-    }
-    return bonusData[0] + ' / ' + bonusData[1] + ' = ' + this.round(bonusData[2]) + '%'
-  }
 
   render() {
-
     return (
       <View>
         {/* Match Stats */}
         <TableSectionHeader>Match Stats</TableSectionHeader>
 
-        {/* TOOD: Push these out to matches... */}
         <InsightRow title='Highest Pressure (kPa)'
-                    qual={'Quals: ' + this.highScoreString(this.props.qual.high_kpa)}
-                    playoff={'Playoffs: ' + this.highScoreString(this.props.playoff.high_kpa)}/>
+                    qual={'Quals: ' + highScoreString(this.props.qual.high_kpa)}
+                    playoff={'Playoffs: ' + highScoreString(this.props.playoff.high_kpa)}/>
 
         <InsightRow title='High Score'
                     qual={'Quals: ' + this.props.qual.high_score[0] + ' in ' + this.props.qual.high_score[2]}
                     playoff={'Playoffs: ' + this.props.playoff.high_score[0] + ' in ' + this.props.playoff.high_score[2]}/>
 
         <InsightRow title='Average Match Score'
-                    qual={this.round(this.props.qual.average_score)}
-                    playoff={this.round(this.props.playoff.average_score)}/>
+                    qual={round(this.props.qual.average_score)}
+                    playoff={round(this.props.playoff.average_score)}/>
 
         <InsightRow title='Average Winning Score'
-                    qual={this.round(this.props.qual.average_win_score)}
-                    playoff={this.round(this.props.playoff.average_win_score)}/>
+                    qual={round(this.props.qual.average_win_score)}
+                    playoff={round(this.props.playoff.average_win_score)}/>
 
         <InsightRow title='Average Win Margin'
-                    qual={this.round(this.props.qual.average_win_margin)}
-                    playoff={this.round(this.props.playoff.average_win_margin)}/>
+                    qual={round(this.props.qual.average_win_margin)}
+                    playoff={round(this.props.playoff.average_win_margin)}/>
 
         <InsightRow title='Average Mobility Points'
-                    qual={this.round(this.props.qual.average_mobility_points_auto)}
-                    playoff={this.round(this.props.playoff.average_mobility_points_auto)}/>
+                    qual={round(this.props.qual.average_mobility_points_auto)}
+                    playoff={round(this.props.playoff.average_mobility_points_auto)}/>
 
         <InsightRow title='Average Rotor Points'
-                    qual={this.round(this.props.qual.average_rotor_points)}
-                    playoff={this.round(this.props.playoff.average_rotor_points)}/>
+                    qual={round(this.props.qual.average_rotor_points)}
+                    playoff={round(this.props.playoff.average_rotor_points)}/>
 
         <InsightRow title='Average Fuel Points'
-                    qual={this.round(this.props.qual.average_fuel_points)}
-                    playoff={this.round(this.props.playoff.average_fuel_points)}/>
+                    qual={round(this.props.qual.average_fuel_points)}
+                    playoff={round(this.props.playoff.average_fuel_points)}/>
 
         <InsightRow title='Average High Goal'
-                    qual={this.round(this.props.qual.average_high_goals)}
-                    playoff={this.round(this.props.playoff.average_high_goals)}/>
+                    qual={round(this.props.qual.average_high_goals)}
+                    playoff={round(this.props.playoff.average_high_goals)}/>
 
         <InsightRow title='Average Low Goal'
-                    qual={this.round(this.props.qual.average_low_goals)}
-                    playoff={this.round(this.props.playoff.average_low_goals)}/>
+                    qual={round(this.props.qual.average_low_goals)}
+                    playoff={round(this.props.playoff.average_low_goals)}/>
 
         <InsightRow title='Average Takeoff (Climb) Points'
-                    qual={this.round(this.props.qual.average_takeoff_points_teleop)}
-                    playoff={this.round(this.props.playoff.average_takeoff_points_teleop)}/>
+                    qual={round(this.props.qual.average_takeoff_points_teleop)}
+                    playoff={round(this.props.playoff.average_takeoff_points_teleop)}/>
 
         <InsightRow title='Average Foul Score'
-                    qual={this.round(this.props.qual.average_foul_score)}
-                    playoff={this.round(this.props.playoff.average_foul_score)}/>
+                    qual={round(this.props.qual.average_foul_score)}
+                    playoff={round(this.props.playoff.average_foul_score)}/>
 
         {/* Match Stats */}
         <TableSectionHeader>Bonus Stats (# successful / # opportunities)</TableSectionHeader>
 
         <InsightRow title='Auto Mobility'
-                    qual={this.bonusStat(this.props.qual.mobility_counts)}
-                    playoff={this.bonusStat(this.props.playoff.mobility_counts)}/>
+                    qual={bonusStat(this.props.qual.mobility_counts)}
+                    playoff={bonusStat(this.props.playoff.mobility_counts)}/>
 
         <InsightRow title='Teleop Takeoff (Climb)'
-                    qual={this.bonusStat(this.props.qual.takeoff_counts)}
-                    playoff={this.bonusStat(this.props.playoff.takeoff_counts)}/>
+                    qual={bonusStat(this.props.qual.takeoff_counts)}
+                    playoff={bonusStat(this.props.playoff.takeoff_counts)}/>
 
         <InsightRow title='Pressure Bonus (kPa Achieved)'
-                    qual={this.bonusStat(this.props.qual.kpa_achieved)}
-                    playoff={this.bonusStat(this.props.playoff.kpa_achieved)}/>
+                    qual={bonusStat(this.props.qual.kpa_achieved)}
+                    playoff={bonusStat(this.props.playoff.kpa_achieved)}/>
 
         <InsightRow title='Rotor 1 Engaged (Auto)'
-                    qual={this.bonusStat(this.props.qual.rotor_1_engaged_auto)}
-                    playoff={this.bonusStat(this.props.playoff.rotor_1_engaged_auto)}/>
+                    qual={bonusStat(this.props.qual.rotor_1_engaged_auto)}
+                    playoff={bonusStat(this.props.playoff.rotor_1_engaged_auto)}/>
 
         <InsightRow title='Rotor 2 Engaged (Auto)'
-                    qual={this.bonusStat(this.props.qual.rotor_2_engaged_auto)}
-                    playoff={this.bonusStat(this.props.playoff.rotor_2_engaged_auto)}/>
+                    qual={bonusStat(this.props.qual.rotor_2_engaged_auto)}
+                    playoff={bonusStat(this.props.playoff.rotor_2_engaged_auto)}/>
 
         <InsightRow title='Rotor 1 Engaged'
-                    qual={this.bonusStat(this.props.qual.rotor_1_engaged)}
-                    playoff={this.bonusStat(this.props.playoff.rotor_1_engaged)}/>
+                    qual={bonusStat(this.props.qual.rotor_1_engaged)}
+                    playoff={bonusStat(this.props.playoff.rotor_1_engaged)}/>
 
         <InsightRow title='Rotor 2 Engaged'
-                    qual={this.bonusStat(this.props.qual.rotor_2_engaged)}
-                    playoff={this.bonusStat(this.props.playoff.rotor_2_engaged)}/>
+                    qual={bonusStat(this.props.qual.rotor_2_engaged)}
+                    playoff={bonusStat(this.props.playoff.rotor_2_engaged)}/>
 
         <InsightRow title='Rotor 3 Engaged'
-                    qual={this.bonusStat(this.props.qual.rotor_3_engaged)}
-                    playoff={this.bonusStat(this.props.playoff.rotor_3_engaged)}/>
+                    qual={bonusStat(this.props.qual.rotor_3_engaged)}
+                    playoff={bonusStat(this.props.playoff.rotor_3_engaged)}/>
 
         <InsightRow title='Rotor 4 Engaged'
-                    qual={this.bonusStat(this.props.qual.rotor_4_engaged)}
-                    playoff={this.bonusStat(this.props.playoff.rotor_4_engaged)}/>
+                    qual={bonusStat(this.props.qual.rotor_4_engaged)}
+                    playoff={bonusStat(this.props.playoff.rotor_4_engaged)}/>
 
       </View>
     );
