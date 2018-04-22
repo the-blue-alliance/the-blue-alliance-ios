e3252e18354756dfd4b6171200b13018c08f02b0
Zach Orr <zachorr139@gmail.com>
2018-04-22 12:51:36 -0400

    Fix deprecated characters

--- the-blue-alliance-ios/Extensions/StringExtension.swift
+++ the-blue-alliance-ios/Extensions/StringExtension.swift
@@ -11,7 +11,7 @@ import Foundation
 extension String {
     
     func prefixTrim(_ prefix: String) -> String {
-        if let index = self.characters.index(where: {!prefix.characters.contains($0)}) {
+        if let index = self.index(where: {!prefix.contains($0)}) {
             return String(self[index..<self.endIndex])
         } else {
             return self
