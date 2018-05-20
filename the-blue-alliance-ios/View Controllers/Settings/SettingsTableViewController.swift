d47bf37aea558e36f153f03cd3166adffc53a5d1
Anas Merbouh <anas.merbouh@outlook.com>
2018-05-20 13:27:26 -0400

    Created the SettingsVC and fixed a bug where auto-sizing cells don't work

---
+++ the-blue-alliance-ios/View Controllers/Settings/SettingsTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SettingsTableViewController.swift
+//  The Blue Alliance
+//
+//  Created by Chaima Merbouh on 18-05-20.
+//  Copyright © 2018 The Blue Alliance. All rights reserved.
+//
+
+import Foundation
---
+++ the-blue-alliance-ios/View Elements/Settings/SwitchTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  SwitchTableViewCell.swift
+//  The Blue Alliance
+//
+//  Created by Chaima Merbouh on 18-05-20.
+//  Copyright © 2018 The Blue Alliance. All rights reserved.
+//
+
+import Foundation
