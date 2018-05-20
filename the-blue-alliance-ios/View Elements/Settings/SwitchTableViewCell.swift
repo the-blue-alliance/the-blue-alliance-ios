3f835826e868cf589c675326d38db5cde6e2acc4
Anas Merbouh <anas.merbouh@outlook.com>
2018-05-20 13:59:41 -0400

    added the video player button image asset and SettingsVC logic

--- the-blue-alliance-ios/View Elements/Settings/SwitchTableViewCell.swift
+++ the-blue-alliance-ios/View Elements/Settings/SwitchTableViewCell.swift
@@ -2,8 +2,19 @@
 //  SwitchTableViewCell.swift
 //  The Blue Alliance
 //
-//  Created by Chaima Merbouh on 18-05-20.
+//  Created by Anas Merbouh on 18-05-20.
 //  Copyright © 2018 The Blue Alliance. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class SwitchTableViewCell: UITableViewCell {
+    
+    @IBOutlet public weak var titleLabel: UILabel!
+    @IBOutlet public weak var switchToggle: UISwitch!
+    
+    public final func populate(withTitle title: String) {
+        titleLabel.text = title
+    }
+    
+}
