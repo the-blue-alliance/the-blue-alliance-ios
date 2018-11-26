b8fe49dfed4dea88490a93773b0adf3177190bca
ZachOrr <ZachOrr@users.noreply.github.com>
2018-11-26 17:56:19 -0500

    Add Subscribable protocol for MyTBA preferences

---
+++ the-blue-alliance-ios/Protocols/Subscribable.swift
@@ -0,0 +1,25 @@
+import Foundation
+import UIKit
+
+// Subscribable is a protocol view controllers can conform to if they want to have
+// the UI to allow users to subscribe to notifications for models
+// Ex: The 'Event' view controller conforms to Subscribable, which shows the subscribe UI
+
+protocol Subscribable {
+    var myTBA: MyTBA { get }
+    var favoriteBarButtonItem: UIBarButtonItem { get }
+    var subscribableModel: MyTBASubscribable { get }
+}
+
+extension Subscribable where Self: UIViewController, Self: Persistable {
+
+    func presentMyTBAPreferences() {
+        let myTBAPreferencesViewController = MyTBAPreferenceViewController(subscribableModel: subscribableModel,
+                                                                           myTBA: myTBA,
+                                                                           persistentContainer: persistentContainer)
+        let navController = UINavigationController(rootViewController: myTBAPreferencesViewController)
+
+        present(navController, animated: true, completion: nil)
+    }
+
+}
