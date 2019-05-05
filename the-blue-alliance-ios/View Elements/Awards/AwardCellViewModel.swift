6a7c5e021ef8d940610abc2c970b9fed75a3e34a
kevinbai0 <kevin.c.bai0@gmail.com>
2019-05-05 14:27:58 -0400

    Added more detail to Recipient

--- the-blue-alliance-ios/View Elements/Awards/AwardCellViewModel.swift
+++ the-blue-alliance-ios/View Elements/Awards/AwardCellViewModel.swift
@@ -3,6 +3,9 @@ import Foundation
 struct Recipient {
     let teamKey: String?
     let awardText: [String]
+    let teamNumber: String?
+    let teamName: String?
+    let awardee: String?
 }
 
 struct AwardCellViewModel {
@@ -14,7 +17,8 @@ struct AwardCellViewModel {
     init(award: Award) {
         awardName = award.name
         recipients = (award.recipients?.allObjects as? [AwardRecipient])?.map({ (recipient) -> Recipient in
-            return Recipient(teamKey: recipient.teamKey?.key, awardText: recipient.awardText)
+            let name = recipient.teamKey?.team?.nickname ?? recipient.teamKey?.team?.name
+            return Recipient(teamKey: recipient.teamKey?.key, awardText: recipient.awardText, teamNumber: recipient.teamKey?.teamNumber, teamName: name, awardee: recipient.awardee)
         }) ?? []
     }
 }
