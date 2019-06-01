e3c5e6e62e1e7d950c277051a92019709c1c70d5
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2019-05-31 21:51:43 -0400

    Change myTBA URL

--- Frameworks/MyTBAKit/Sources/Classes/MyTBA.swift
+++ Frameworks/MyTBAKit/Sources/Classes/MyTBA.swift
@@ -4,7 +4,7 @@ public typealias MyTBARequestCompletionBlock = (_ data: Data?, _ error: Error?) 
 
 private struct Constants {
     struct APIConstants {
-        static let baseURL = URL(string: "https://tbatv-prod-hrd.appspot.com/clientapi/tbaClient/v9/")!
+        static let baseURL = URL(string: "https://www.thebluealliance.com/clientapi/tbaClient/v9/")!
     }
 }
 
