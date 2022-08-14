7b328edeb86884e1d3b802358074a137e4b57028
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2022-08-14 12:18:30 -0400

    Flatten app setup operations using async/await (#941)

--- the-blue-alliance-ios/Operations/App Setup/AppSetupOperation.swift
+++ the-blue-alliance-ios/Operations/App Setup/AppSetupOperation.swift
@@ -2,36 +2,21 @@ import CoreData
 import Foundation
 import Search
 import TBAKit
-import TBAOperation
-import UIKit
 
-class AppSetupOperation: TBAOperation {
+struct AppSetupOperation {
 
-    var destroyPersistentStoreOperation: DestroyPersistentStoreOperation
-    var persistentContainerOperation: PersistentContainerOperation
-
-    let appSetupOperationQueue = OperationQueue()
+    private let destroyPersistentStoreOperation: DestroyPersistentStoreOperation
+    private let persistentContainerOperation: PersistentContainerOperation
 
     init(indexDelegate: TBACoreDataCoreSpotlightDelegate, persistentContainer: NSPersistentContainer, tbaKit: TBAKit, userDefaults: UserDefaults) {
         self.destroyPersistentStoreOperation = DestroyPersistentStoreOperation(persistentContainer: persistentContainer, tbaKit: tbaKit, userDefaults: userDefaults)
 
         self.persistentContainerOperation = PersistentContainerOperation(indexDelegate: indexDelegate, persistentContainer: persistentContainer)
-        self.persistentContainerOperation.addDependency(self.destroyPersistentStoreOperation)
-
-        super.init()
     }
 
-    override func execute() {
-        let blockOperation = BlockOperation { [unowned self] in
-            self.completionError = [self.destroyPersistentStoreOperation, self.persistentContainerOperation].compactMap({ $0.completionError }).first
-            self.finish()
-        }
-
-        let dependentOperations = [destroyPersistentStoreOperation, persistentContainerOperation]
-        for op in dependentOperations {
-            blockOperation.addDependency(op)
-        }
-        appSetupOperationQueue.addOperations(dependentOperations + [blockOperation], waitUntilFinished: false)
+    func execute() async throws {
+        try destroyPersistentStoreOperation.execute()
+        _ = try await persistentContainerOperation.execute()
     }
 
 }
