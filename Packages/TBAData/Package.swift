73336e7cc73dafdb0c59c9678a2d4a1463d12a19
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2024-03-19 16:35:24 -0400

    Bump minimum deploy target to iOS 17

--- Packages/TBAData/Package.swift
+++ Packages/TBAData/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "TBAData",
     platforms: [
-        .iOS(.v15),
+        .iOS(.v17),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
