71c8d3d4a17f4646fc8dcfb4e7a48ea99cf2d19d
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2025-11-16 13:19:30 -0500

    Some updates

---
+++ TBATests/TBATests.swift
@@ -0,0 +1,18 @@
+//
+//  TBATests.swift
+//  TBATests
+//
+//  Created by Zachary Orr on 11/16/25.
+//  Copyright © 2025 The Blue Alliance. All rights reserved.
+//
+
+import Testing
+@testable import TBA
+
+struct TBATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
