e7fc95a938b4224c0d96de328192ea2843714113
owennkuo <owennkuo@gmail.com>
2026-04-14 22:31:41 -0700

    add totals and auto climb row

--- the-blue-alliance-ios/ViewElements/Match/Breakdown/MatchBreakdownConfigurator2026.swift
+++ the-blue-alliance-ios/ViewElements/Match/Breakdown/MatchBreakdownConfigurator2026.swift
@@ -16,7 +16,7 @@ struct MatchBreakdownConfigurator2026: MatchBreakdownConfigurator {
         var rows: [BreakdownRow?] = []
         
         // Auto
-        rows.append(nestedRow(title: "Auto Tower", keyPath: ["hubScore", "autoPoints"], red: red, blue: blue))
+        rows.append(autoClimb(red: red, blue: blue))
         rows.append(row(title: "Auto Tower Points", key: "autoTowerPoints", red: red, blue: blue))
         rows.append(nestedRow(title: "Auto Fuel", keyPath: ["hubScore", "autoPoints"], red: red, blue: blue))
         rows.append(row(title: "Total Auto", key: "totalAutoPoints", red: red, blue: blue, type:.total))
@@ -37,12 +37,18 @@ struct MatchBreakdownConfigurator2026: MatchBreakdownConfigurator {
         rows.append(row(title: "Total Tower Points", key: "totalTowerPoints", red: red, blue: blue, type:.subtotal))
         rows.append(nestedRow(title: "Total Fuel Points", keyPath: ["hubScore", "totalPoints"], red: red, blue: blue, type:.subtotal))
         rows.append(row(title: "Total Teleop", key: "totalTeleopPoints", red: red, blue: blue, type:.total))
-        // RP/Fouls
+        // RP
         rows.append(boolImageRow(title: "Engergized Bonus", key: "energizedAchieved", red: red, blue: blue))
         rows.append(boolImageRow(title: "Supercharged Bonus", key: "superchargedAchieved", red: red, blue: blue))
         rows.append(boolImageRow(title: "Traversal Bonus", key: "traversalAchieved", red: red, blue: blue))
+        // Fouls/Total
         rows.append(foulRow(title: "Fouls / Major Fouls", red: red, blue: blue))
         rows.append(row(title: "Foul Points", key: "foulPoints", red: red, blue: blue, type:.subtotal))
+        rows.append(row(title: "Adjustments", key: "adjustPoints", red: red, blue: blue))
+        rows.append(row(title: "Total Score", key: "totalPoints", red: red, blue: blue, type:.total))
+        rows.append(row(title: "Ranking Points", key: "rp", formatString: "+%@ RP", red: red, blue: blue))
+
+
         // Clean up any empty rows
         let validRows = rows.compactMap({ $0 })
         if !validRows.isEmpty {
@@ -58,7 +64,7 @@ struct MatchBreakdownConfigurator2026: MatchBreakdownConfigurator {
         guard let redFouls = rf as? Int, let blueFouls = bf as? Int else {
             return nil
         }
-
+        
         guard let minorFoulValues = values(key: "majorFoulCount", red: red, blue: blue) else {
             return nil
         }
@@ -66,11 +72,53 @@ struct MatchBreakdownConfigurator2026: MatchBreakdownConfigurator {
         guard let redMajorFouls = rmf as? Int, let blueMajorFouls = bmf as? Int else {
             return nil
         }
-
+        
         let elements = [(redFouls, redMajorFouls), (blueFouls, blueMajorFouls)].map { (fouls, majorFouls) -> AnyHashable in
             return "\(fouls) / \(majorFouls)"
         }
         return BreakdownRow(title: title, red: [elements.first], blue: [elements.last])
     }
+    
+    private static func autoClimb(red: [String: Any]?, blue: [String: Any]?) -> BreakdownRow? {
+        var redClimbStrings: [String] = []
+        var blueClimbStrings: [String] = []
+        
+        for i in [1, 2, 3] {
+            guard let taxiValues = values(key: "autoTowerRobot\(i)", red: red, blue: blue) else {
+                return nil
+            }
+            let (rv, bv) = taxiValues
+            guard let redClimb = rv as? String, let blueClimb = bv as? String else {
+                return nil
+            }
+            redClimbStrings.append(redClimb)
+            blueClimbStrings.append(blueClimb)
+        }
+        
+        let mode = UIView.ContentMode.scaleAspectFit
+        let elements = [redClimbStrings, blueClimbStrings].map { (climbStrings) -> [AnyHashable] in
+            return climbStrings.map { (climb) -> AnyHashable in
+                switch climb {
+                case "None":
+                    return BreakdownStyle.imageView(image: BreakdownStyle.xImage, contentMode: mode, forceSquare: false)
+                case "Level1":
+                    return BreakdownStyle.imageView(image: BreakdownStyle.checkImage, contentMode: mode, forceSquare: false)
+                default:
+                    return "?"
+                }
+            }
+        }
+        let (redElements, blueElements) = (elements[0], elements[1])
+        guard let redBreakdownElements = redElements as? [BreakdownElement], let blueBreakdownElements = blueElements as? [BreakdownElement] else {
+            return nil
+        }
 
+        let redStackView = UIStackView(arrangedSubviews: redBreakdownElements.map { $0.toView() })
+        redStackView.distribution = .fillEqually
+        let blueStackView = UIStackView(arrangedSubviews: blueBreakdownElements.map { $0.toView() })
+        blueStackView.distribution = .fillEqually
+
+        return BreakdownRow(title: "Auto Tower", red: [redStackView], blue: [blueStackView], type: .subtotal)
+
+    }
 }
