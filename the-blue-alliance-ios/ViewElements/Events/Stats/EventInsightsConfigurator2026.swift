1dd019f78b062b7e46d4880f1cb7b15171dbf35d
owennkuo <owennkuo@gmail.com>
2026-04-18 14:23:11 -0700

    2026 insights

--- the-blue-alliance-ios/ViewElements/Events/Stats/EventInsightsConfigurator2026.swift
+++ the-blue-alliance-ios/ViewElements/Events/Stats/EventInsightsConfigurator2026.swift
@@ -1,7 +1,7 @@
 import Foundation
 import UIKit
 
-class EventInsightsConfigurator2022: EventInsightsConfigurator {
+class EventInsightsConfigurator2026: EventInsightsConfigurator {
 
     static func configureDataSource(_ snapshot: inout NSDiffableDataSourceSnapshot<String, InsightRow>, _ qual: [String : Any]?, _ playoff: [String : Any]?) {
         // Match Stats
@@ -11,13 +11,6 @@ class EventInsightsConfigurator2022: EventInsightsConfigurator {
         matchStats.append(scoreRow(title: "Average Match Score", key: "average_score", qual: qual, playoff: playoff))
         matchStats.append(scoreRow(title: "Average Winning Score", key: "average_win_score", qual: qual, playoff: playoff))
         matchStats.append(scoreRow(title: "Average Win Margin", key: "average_win_margin", qual: qual, playoff: playoff))
-        matchStats.append(scoreRow(title: "Average Taxi Points", key: "average_taxi_points", qual: qual, playoff: playoff))
-        matchStats.append(scoreRow(title: "Average Cargo Count (Lower)", key: "average_lower_cargo_count", qual: qual, playoff: playoff))
-        matchStats.append(scoreRow(title: "Average Cargo Count (Upper)", key: "average_upper_cargo_count", qual: qual, playoff: playoff))
-        matchStats.append(scoreRow(title: "Average Cargo Count", key: "average_cargo_count", qual: qual, playoff: playoff))
-        matchStats.append(scoreRow(title: "Average Cargo Points", key: "average_cargo_points", qual: qual, playoff: playoff))
-        matchStats.append(scoreRow(title: "Average Hangar Points", key: "average_endgame_points", qual: qual, playoff: playoff))
-        matchStats.append(scoreRow(title: "Average Foul Points", key: "average_foul_score", qual: qual, playoff: playoff))
         // Note - this is not helpful, since we already show "Average Match Score"
         // If we can show breakdowns by Auto/Teleop/Overall like on web, we can add this back
         // https://github.com/the-blue-alliance/the-blue-alliance-ios/issues/744
@@ -32,20 +25,25 @@ class EventInsightsConfigurator2022: EventInsightsConfigurator {
         // Bonus Stats
         var bonusStats: [InsightRow] = []
 
-        bonusStats.append(bonusRow(title: "Taxi", key: "taxi_count", qual: qual, playoff: playoff))
-        bonusStats.append(bonusRow(title: "Quintet Achieved", key: "quintet_count", qual: qual, playoff: playoff))
-        bonusStats.append(bonusRow(title: "Low Climb", key: "low_climb_count", qual: qual, playoff: playoff))
-        bonusStats.append(bonusRow(title: "Mid Climb", key: "mid_climb_count", qual: qual, playoff: playoff))
-        bonusStats.append(bonusRow(title: "High Climb", key: "high_climb_count", qual: qual, playoff: playoff))
-        bonusStats.append(bonusRow(title: "Traversal Climb", key: "traversal_climb_count", qual: qual, playoff: playoff))
-        bonusStats.append(bonusRow(title: "Cargo Bonus RP Achieved", key: "cargo_bonus_rp", qual: qual, playoff: playoff))
-        bonusStats.append(bonusRow(title: "Hangar Bonus RP Achieved", key: "hangar_bonus_rp", qual: qual, playoff: playoff))
-        bonusStats.append(bonusRow(title: "\"Unicorn Matches\" (Win + Cargo Bonus + Hangar Bonus)", key: "unicorn_matches", qual: qual, playoff: playoff))
+        bonusStats.append(bonusRow(title: "Energized RP", key: "energized_rp_count", qual: qual, playoff: nil))
+        bonusStats.append(bonusRow(title: "Supercharged RP", key: "supercharged_rp_count", qual: qual, playoff: nil))
+        bonusStats.append(bonusRow(title: "Traversal RP", key: "traversal_rp_count", qual: qual, playoff: nil))
+        bonusStats.append(bonusRow(title: "6 RP", key: "six_rp_count", qual: qual, playoff: nil))
+        bonusStats.append(bonusRow(title: "9 RP", key: "nine_rp_count", qual: qual, playoff: nil))
+        bonusStats.append(bonusRow(title: "Auto Win Conversion", key: "auto_win_conversion", qual: qual, playoff: playoff))
 
         bonusStats = filterEmptyInsights(bonusStats)
         if !bonusStats.isEmpty {
-            snapshot.appendSections(["Bonus Stats (# successful / # opportunities)"])
-            snapshot.appendItems(bonusStats, toSection: "Bonus Stats (# successful / # opportunities)")
+            snapshot.appendSections(["Bonus Statistics"])
+            snapshot.appendItems(bonusStats, toSection: "Bonus Statistics")
+        }
+        var fuelStats: [InsightRow] = []
+
+        fuelStats.append(scoreRow(title: "Auto Fuel Scored", key: "auto_fuel_scored", qual: qual, playoff: playoff))
+        fuelStats = filterEmptyInsights(fuelStats)
+        if !fuelStats.isEmpty {
+            snapshot.appendSections(["Fuel Statiestics"])
+            snapshot.appendItems(fuelStats, toSection: "Fuel Statistics")
         }
     }
 
