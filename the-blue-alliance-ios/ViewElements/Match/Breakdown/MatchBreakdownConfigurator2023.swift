52f290377a4a0d3f6457a03a823d2c0ee9981672
owennkuo <owennkuo@gmail.com>
2026-05-23 19:11:24 -0700

    run swift-format

--- the-blue-alliance-ios/ViewElements/Match/Breakdown/MatchBreakdownConfigurator2023.swift
+++ the-blue-alliance-ios/ViewElements/Match/Breakdown/MatchBreakdownConfigurator2023.swift
@@ -150,7 +150,8 @@ struct MatchBreakdownConfigurator2023: MatchBreakdownConfigurator {
         }
 
         let mode = UIView.ContentMode.scaleAspectFit
-        let elements = [redLeaveStrings, blueLeaveStrings].map { (mobilityStrings) -> [AnyHashable] in
+        let elements = [redLeaveStrings, blueLeaveStrings].map {
+            (mobilityStrings) -> [AnyHashable] in
             return mobilityStrings.map { (mobility) -> AnyHashable in
                 switch mobility {
                 case "No":
